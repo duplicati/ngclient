b285048253c1edec7eb5171ee1179fa8a4765cd0
sp90 <dev@nomis.dk>
2024-10-23 11:27:10 +0200

    Add initial support for recieving config relayConfig

--- src/app/core/states/relayconfig.state.ts
+++ src/app/core/states/relayconfig.state.ts
@@ -13,6 +13,58 @@ export class RelayconfigState {
 
   config = this.#config.asReadonly();
 
+  parentIframeListner: AbortController | null = null;
+  parentOrigin = '*';
+
+  fetchConfig(): void {
+    this.#killListener();
+
+    this.parentIframeListner = new AbortController();
+    this.#sendMessageToParent('connected');
+
+    window.addEventListener<any>(
+      'message',
+      (event: MessageEvent) => {
+        if (event.data.startsWith('access:')) {
+          const data = event.data.replace('access:', '');
+          const parsed = JSON.parse(data) as { [key: string]: unknown };
+
+          if (!parsed?.['accessToken'] || !parsed?.['clientId']) {
+            this.#sendMessageToParent('error:invalid-config');
+            return;
+          }
+
+          if (typeof parsed['clientId'] !== 'string') {
+            this.#sendMessageToParent('error:invalid-config');
+            return;
+          }
+
+          if (typeof parsed['accessToken'] !== 'string') {
+            this.#sendMessageToParent('error:invalid-config');
+            return;
+          }
+
+          console.info('postMessage config set');
+          this.#config.set(parsed as Relayconfig);
+        }
+      },
+      {
+        signal: this.parentIframeListner.signal,
+      }
+    );
+  }
+
+  #sendMessageToParent(message: string) {
+    window.parent.postMessage(message, '*');
+  }
+
+  #killListener() {
+    if (this.parentIframeListner) {
+      this.parentIframeListner.abort();
+      this.parentIframeListner = null;
+    }
+  }
+
   setConfig(config: Relayconfig) {
     this.#config.set(config);
   }
