c649699d2a499c5096c15d76fc5676a57fade4ec
sp90 <dev@nomis.dk>
2025-01-15 10:31:50 +0100

    Fixes #95

--- src/app/about/system-info/system-info.component.ts
+++ src/app/about/system-info/system-info.component.ts
@@ -1,16 +1,53 @@
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { JsonPipe, NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { SparkleIconComponent, SparkleListComponent } from '@sparkle-ui/core';
 import { SysinfoState } from '../../core/states/sysinfo.state';
 
 @Component({
-    selector: 'app-system-info',
-    imports: [JsonPipe],
-    templateUrl: './system-info.component.html',
-    styleUrl: './system-info.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-system-info',
+  imports: [JsonPipe, SparkleListComponent, SparkleIconComponent, NgTemplateOutlet],
+  templateUrl: './system-info.component.html',
+  styleUrl: './system-info.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class SystemInfoComponent {
   #sysInfo = inject(SysinfoState);
 
-  systemInfo = this.#sysInfo.systemInfo;
+  // systemInfo = this.#sysInfo.systemInfo;
+
+  treeStructure = computed(() => {
+    const sysInfo = this.#sysInfo.systemInfo();
+
+    return this.processObject(sysInfo, 0);
+  });
+
+  toggleNode(node: any) {
+    node.isOpen.update((isOpen: any) => !isOpen);
+  }
+
+  private processObject(obj: any, level: number): any {
+    const children = [];
+
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') {
+        children.push({
+          id: key,
+          text: key,
+          children: this.processObject(obj[key], level + 1),
+          level: level,
+          isOpen: signal(false),
+        });
+      } else {
+        children.push({
+          id: key,
+          text: `${key}: ${obj[key]}`,
+          children: [],
+          level: level,
+          isOpen: signal(false),
+        });
+      }
+    }
+
+    return children;
+  }
 }
