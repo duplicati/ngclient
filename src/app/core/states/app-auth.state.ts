b55c863a00a81d62f15b72a798a1619f2094cd68
Kenneth Skovhede <kenneth@hexad.dk>
2025-01-29 16:09:59 +0100

    Fixed not calling `/refresh` when relay'ed

--- src/app/core/states/app-auth.state.ts
+++ src/app/core/states/app-auth.state.ts
@@ -1,7 +1,10 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { finalize, take, tap } from 'rxjs';
-import { DuplicatiServerService } from '../openapi';
+import { finalize, Observable, take, tap } from 'rxjs';
+import { AccessTokenOutput, DuplicatiServerService } from '../openapi';
+import { RelayconfigState } from './relayconfig.state';
+
+const dummytoken = 'dummytoken';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +12,7 @@ import { DuplicatiServerService } from '../openapi';
 export class AppAuthState {
   #router = inject(Router);
   #dupServer = inject(DuplicatiServerService);
+  #relayConfigState = inject(RelayconfigState);
   #token = signal<string | null>(null);
   #isLoggingOut = signal(false);
 
@@ -30,6 +34,18 @@ export class AppAuthState {
   }
 
   refreshToken() {
+    // In relay mode, the requests are proxied and authenticated by the client
+    // TODO: This should be fixed by probing the API, as we can be
+    // pre-authenticated if running behind a proxy
+    if (this.#relayConfigState.relayIsEnabled()) {
+      this.#token.set(dummytoken);
+
+      return new Observable<AccessTokenOutput>((observer) => {
+        observer.next({ AccessToken: dummytoken });
+        observer.complete();
+      });
+    }
+
     return this.#dupServer.postApiV1AuthRefresh().pipe(
       take(1),
       tap((res) => this.#token.set(res.AccessToken ?? null))
