a3ce84cc62b16497da9a0800c0233563641c5d99
sp90 <dev@nomis.dk>
2025-05-06 12:49:36 +0200

    Support updating app in ngclient

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SparkleTabsComponent } from '@sparkle-ui/core';
+import StatusBarComponent from '../core/components/status-bar/status-bar.component';
 
 @Component({
   selector: 'app-about',
-  imports: [SparkleTabsComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [StatusBarComponent, SparkleTabsComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
