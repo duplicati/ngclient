84db5495ac5810a0846931d36e600e937da41289
sp90 <dev@nomis.dk>
2025-09-12 11:25:46 +0200

    Cleanup config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,21 +3,13 @@ import { defineConfig } from '@hey-api/openapi-ts';
 const localFile = false; // && './swagger-orgid-tokens.json';
 const apiInput = 'http://localhost:8200/swagger/v1/swagger.json';
 export default defineConfig({
-  // client: 'legacy/angular',
   input: localFile || apiInput,
   output: 'projects/ngclient/src/app/core/openapi',
   plugins: [
-    // ...defaultPlugins,
     'legacy/angular',
-    // '@hey-api/typescript',
     {
       asClass: true,
       name: '@hey-api/sdk',
     },
-    // '@angular/common',
   ],
-  // schemas: true,
-  // services: {
-  //   asClass: true,
-  // },
 });
