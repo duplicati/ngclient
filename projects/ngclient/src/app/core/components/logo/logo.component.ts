08ed69fcf1fc7d8981794609219f11110bb3cc14
Kenneth Skovhede <kenneth@duplicati.com>
2025-11-09 17:17:30 +0100

    Added support for custom branding

--- projects/ngclient/src/app/core/components/logo/logo.component.ts
+++ projects/ngclient/src/app/core/components/logo/logo.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
-    selector: 'app-logo',
-    imports: [],
-    templateUrl: './logo.component.html',
-    styleUrl: './logo.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LogoComponent {}
+export default class LogoComponent {
+  customLogo = signal<string | null>(null);
+  customName = signal<string | null>(null);
+
+  ngOnInit() {
+    const wnd = window as any;
+    this.customLogo.set(wnd.BRANDING_LOGO);
+    this.customName.set(wnd.BRANDING_NAME);
+  }
+}
