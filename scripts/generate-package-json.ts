6bd48e5717bb72b53d9084915dcbc8ab4bc684fe
Kenneth Skovhede <kenneth@duplicati.com>
2026-01-29 13:47:39 +0100

    Fixed package gen to include additional fields

--- scripts/generate-package-json.ts
+++ scripts/generate-package-json.ts
@@ -4,12 +4,15 @@ import packageJson from '../package.json';
 import { write } from 'bun';
 
 const newPackageJson = {
-  version: packageJson.version,
   name: packageJson.name,
+  version: packageJson.version,
   license: packageJson.license,
   contributors: packageJson.contributors,
+  repository: packageJson.repository,
+  bugs: packageJson.bugs,
+  homepage: packageJson.homepage,
 };
 
 await write('./dist/ngclient/browser/package.json', JSON.stringify(newPackageJson, null, 2));
 
-console.log('package.json generated');
+console.log('package.json generated with repository metadata');
